b66c223a15f1e56ca68c3d5b9cc8ee6047177fc2
microup <ofp1979@yandex.ru>
2023-02-03 08:48:54 +0300

    added check key exists

--- src/cache.go
+++ src/cache.go
@@ -2,14 +2,24 @@ package cache
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 )
 
+type KeyAlreadyExistsError struct {
+	Key any
+}
+
+func (e *KeyAlreadyExistsError) Error() string {
+	return fmt.Sprintf("key %v already exists", e.Key)
+}
+
+
 type Cache struct {
 	durationTimeEvict   time.Duration
 	durationCheckTicker time.Duration
-	data                map[any] *cacheValue
+	data                map[any]*cacheValue
 	mu                  *sync.RWMutex
 }
 
@@ -21,9 +31,9 @@ type cacheValue struct {
 func New(timeCheckNewTicker time.Duration, timeRecordEvict time.Duration) *Cache {
 	return &Cache{
 		mu:                  &sync.RWMutex{},
-		data:                make(map[any] *cacheValue),
+		data:                make(map[any]*cacheValue),
 		durationCheckTicker: timeCheckNewTicker,
-		durationTimeEvict:   timeRecordEvict,		
+		durationTimeEvict:   timeRecordEvict,
 	}
 }
 
@@ -44,11 +54,17 @@ func (c *Cache) StartEvict(ctx context.Context) {
 	}()
 }
 
-func (c *Cache) Add(key any, value any) {
+func (c *Cache) Add(key any, value any) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
+	if _, ok := c.data[key]; ok {
+		return &KeyAlreadyExistsError{Key: key}
+	}
+
 	c.data[key] = &cacheValue{value: value, lastUsed: time.Now()}
+
+	return nil
 }
 
 func (c *Cache) Get(key any) (any, bool) {
