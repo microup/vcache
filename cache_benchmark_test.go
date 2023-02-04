0f097e8f4bf3472072d4bc4d71e103f07bebc830
microup <ofp1979@yandex.ru>
2023-02-04 08:54:20 +0300

    added BenchmarkCacheMixed and improved BenchmarkCacheDelete

--- cache_benchmark_test.go
+++ cache_benchmark_test.go
@@ -1,6 +1,7 @@
 package cache_test
 
 import (
+	"context"
 	"strconv"
 	"testing"
 	"time"
@@ -26,26 +27,12 @@ func BenchmarkCacheGet(b *testing.B) {
 		value := "value" + strconv.Itoa(i)
 		_ = cacheTest.Add(key, value)
 	}
+
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		key := "key" + strconv.Itoa(i)
 		cacheTest.Get(key)
-	}
-}
-
-func BenchmarkCacheEvict(b *testing.B) {
-	cacheTest := cache.New(1*time.Second, 2*time.Second)
-
-	for i := 0; i < b.N; i++ {
-		key := "key" + strconv.Itoa(i)
-		value := "value" + strconv.Itoa(i)
-		_ = cacheTest.Add(key, value)
-	}
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		cacheTest.Evict()
 	}
 }
 
@@ -57,10 +44,41 @@ func BenchmarkCacheDelete(b *testing.B) {
 		value := "value" + strconv.Itoa(i)
 		_ = cacheTest.Add(key, value)
 	}
+
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		key := "key" + strconv.Itoa(i)
 		cacheTest.Delete(key)
+	}
+}
+
+func BenchmarkCacheEvict(b *testing.B) {
+	cacheTest := cache.New(60*time.Second, 2*time.Second)
+
+	for i := 0; i < b.N; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "value" + strconv.Itoa(i)
+		_ = cacheTest.Add(key, value)
+	}
+
+	// this needs to be done so that the condition for deletion applies to all records.
+	time.Sleep(2 * time.Second)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		cacheTest.Evict()
+	}
+}
+
+func BenchmarkCacheMixed(b *testing.B) {
+	cacheTest := cache.New(1*time.Nanosecond, 1*time.Nanosecond)
+	cacheTest.StartEvict(context.Background())
+
+	for i := 0; i < b.N; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "value" + strconv.Itoa(i)
+		_ = cacheTest.Add(key, value)
 	}
 }
